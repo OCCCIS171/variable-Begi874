89fcda3402fcfa49cde738dbbc1debb6b2ccf19d
Begi874 <158251887+Begi874@users.noreply.github.com>
2024-02-11 20:55:18 -0800

    Update variable.js
    Signed-off-by: Begi874 <158251887+Begi874@users.noreply.github.com>

--- variable.js
+++ variable.js
@@ -1,10 +1,11 @@
 // TODO: Create a variable named `firstName` and assign your first name to it in the line below.
-let firstName = undefined;
+let firstName = Francis;
 
 // TODO: Create a variable named `lastName` and assign your last name to it in the line below.
-
+let lastName = Cyr;
 
 // TODO: Create a variable named `sayHello` and build the following string based on the variables
+let sayHello = "Hello world! My name is "+firstName+" "+lastName+". Nice to meet you!"
 // above: "Hello world! My name is <firstName> <lastName>. Nice to meet you!". 
 
 
